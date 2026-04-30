098708839f6bb5bf5cc1bdc3dc483d6430e1db61
Oriak <diegooriak3@outlook.com>
2026-04-30 17:05:01 -0600

    Phase2 15

--- src/Pages/SelectService.tsx
+++ src/Pages/SelectService.tsx
@@ -2,10 +2,14 @@ import { Box, Button, Card, Grid, InputAdornment, TextField, Typography } from "
 import Header from "../components/Header";
 import MoneyCard from "../components/MoneyCard";
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { giftCards, internet, peaje, productosCatalogo, productosFinacieros, recargas, servicios, tesoreria } from "../data/comisiones";
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 
+type mounts = {
+    label: string;
+    mount: number;
+}
 
 export default function SelectService() {
 
@@ -15,10 +19,20 @@ export default function SelectService() {
         number: number;
         image: string;
         action?: () => void;
-    }
+    };
+
+    type service = {
+    service: data['label'];
+    type: 'recargas' | 'servicios' | 'tarjetas-regalo' | 'peaje' | 'internet' | 'tesoreria' | 'productos-financieros' | 'productos-catalogo';
+    mounts?: mounts[];
+    referenceImage?: string;
+    helpText?: string;
+};
 
     const [services, setServices] = useState<data[] | null>(null);
+    const [serv, setServ] = useState<service[] | null>(null); 
     const location = useLocation();
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (location.pathname === '/recargas') {
@@ -47,6 +61,21 @@ export default function SelectService() {
         }
     }, [location]);
 
+    const handleNav = (s: service) => {
+        const look = services?.find(c => c.label === s.service);
+        if (look) {
+            setServ([s]);
+        };
+
+        if (s.type === 'recargas' || s.type === 'peaje' || s.type === 'tarjetas-regalo' || s.type === 'internet' || s.service === 'Pospago') {
+            return 'hola';
+        };
+        
+        if (s.type === 'productos-financieros' || s.type === 'productos-catalogo' || s.type === 'tesoreria') {
+            return 'mundo';
+        }
+    }
+
     return (
         <>
             <Header />
@@ -67,7 +96,7 @@ export default function SelectService() {
                             <Card variant="outlined" sx={{ backgroundColor: '#C29B33', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 2, p: 3, borderRadius: '9px', overflow: 'hidden', width: '70%' }}>
                                 <Box component={'img'} src={c.image} alt='MainImage' sx={{ width: 'auto', height: 'auto', borderRadius: 50, backgroundColor: '#ffffff', }} />
                                 <Typography variant="h5" color="#000000" fontWeight={'bold'} fontFamily={'system-ui'}>{c.label}</Typography>
-                                <Button variant="contained" size="large" onClick={c.action} sx={{ backgroundColor: '#D04234', color: '#ffffff', fontFamily: 'system-ui', width: '100%', fontSize: '15px', fontWeight: 'bold', borderRadius: '50px' }}>Comenzar</Button>
+                                <Button variant="contained" size="large" sx={{ backgroundColor: '#D04234', color: '#ffffff', fontFamily: 'system-ui', width: '100%', fontSize: '15px', fontWeight: 'bold', borderRadius: '50px' }}>Comenzar</Button>
                             </Card>
                         </Grid>
                     ))}
